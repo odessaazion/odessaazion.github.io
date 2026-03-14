f94eebb521b129037024a75c846e22e098ad9849
999 <n072ar.s@gmail.com>
2026-03-14 23:31:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -261,6 +261,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const bioMessages = [
     "owner .gg/odessaazion, also a big juice wrld fan",
+    "this website is insanely broken on mobile (no i wont be fixing it)",
     "\"hello!!!\"",
     "real ass mf",
     "\"song playing; Lean Wit' Me (Studio Session) - Juice WRLD\""
